72e16221233d38ecc12193f3b38441f246807487
Sean Meng <seanmeng2018@gmail.com>
2024-08-04 14:57:56 -0400

    Added Some things

---
+++ cruonch.js
@@ -0,0 +1,16 @@
+const blueAllianceApi = "";
+const blueAllianceKey = "";
+
+const geocodingApi = "";
+const geocodingKey = "";
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+    });
+  } else {
+    console.log("Geolocation is not supported by this browser.");
+  }
+  
--- map.js
+++ map.js
@@ -1,6 +1,9 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([44, -85.2], 7);
 
 L.tileLayer('https://tile.thunderforest.com/atlas/{z}/{x}/{y}.png?apikey=99446c586a174ce29291d944db9e2088', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+var marker = L.marker([44, -85.2]).addTo(map);
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.");
